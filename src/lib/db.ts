df40c5e8c45edcf47f3f6063209ae0f9dae9b4c3
Pooja <pooja.shah@techifysolution.com>
2025-07-01 14:50:35 +0530

    db 2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,7 @@ import path from 'path';
 
 const pool = new Pool({
   connectionString: process.env.AZURE_POSTGRES_CONNECTION_STRING,
-  ssl: false,
+  ssl: false as any,
 });
 
 export default pool;
