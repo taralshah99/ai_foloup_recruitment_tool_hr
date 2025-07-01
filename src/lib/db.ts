8d3d4d5f307e3c0c80ff0b189a3f7938fef91688
Pooja <pooja.shah@techifysolution.com>
2025-07-01 13:59:23 +0530

    db updates

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,11 +5,15 @@ import path from 'path';
 const caCert = fs.readFileSync('/home/azureuser/techifysolutions-ssl.crt').toString();
 
 const pool = new Pool({
-  connectionString: process.env.AZURE_POSTGRES_CONNECTION_STRING,
+  host: '10.1.0.17',
+  port: 5432,
+  user: 'hruser',
+  password: 'T@chify$ol8m0s0!',
+  database: 'hrtest',
   ssl: {
     ca: caCert,
-    rejectUnauthorized: true,
-  } as any, // Typecast to fix linter error
+    rejectUnauthorized: true
+  } as any,
 });
 
 export default pool;
