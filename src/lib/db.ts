fd8c1aa40c1d08fd391c0950a652e616dc544694
Pooja <pooja.shah@techifysolution.com>
2025-07-01 14:48:13 +0530

    db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,11 @@ import { Pool } from 'pg';
 import fs from 'fs';
 import path from 'path';
 
-const caCert = fs.readFileSync('/home/azureuser/techifysolutions-ssl.crt').toString();
+// const caCert = fs.readFileSync('/home/azureuser/techifysolutions-ssl.crt').toString();
 
 const pool = new Pool({
   connectionString: process.env.AZURE_POSTGRES_CONNECTION_STRING,
-  ssl: {
-    ca: caCert,
-    rejectUnauthorized: true, // 🔒 Enforce certificate verification
-  } as any,
+  ssl: false,
 });
 
 export default pool;
